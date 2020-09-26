4130f8564f855e2c9c8bb1eb5dcf283d39ca3a76
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-09-26 12:02:17 +0200

    First stage of workshop

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.css';
+
+function Header({score}) {
+    return (
+        <header className="header">
+            <span>SCORE: {score}</span>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    score: 0
+}
+
+export default Header;
