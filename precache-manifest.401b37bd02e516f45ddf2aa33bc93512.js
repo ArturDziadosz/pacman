4aa3ac35f5b8d1dadfd355e605774f10de70daec
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-09-29 13:19:00 +0200

    Updates

---
+++ precache-manifest.401b37bd02e516f45ddf2aa33bc93512.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531ff04c676d1ea94351da1a38de57a4",
+    "url": "/pacman/index.html"
+  },
+  {
+    "revision": "c590d2c447d7a463fde4",
+    "url": "/pacman/static/css/main.bb11163f.chunk.css"
+  },
+  {
+    "revision": "9d118e267794fb82ebc7",
+    "url": "/pacman/static/js/2.cb3068d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pacman/static/js/2.cb3068d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c590d2c447d7a463fde4",
+    "url": "/pacman/static/js/main.f61821b2.chunk.js"
+  },
+  {
+    "revision": "802522991228d2e9abc8",
+    "url": "/pacman/static/js/runtime-main.31137a62.js"
+  },
+  {
+    "revision": "c504eec39be72b7da2e814f70e7b3e66",
+    "url": "/pacman/static/media/ghost.c504eec3.svg"
+  },
+  {
+    "revision": "03edcd8c84e8e3661ef8d5819d2f0ec4",
+    "url": "/pacman/static/media/pacman.03edcd8c.svg"
+  }
+]);
